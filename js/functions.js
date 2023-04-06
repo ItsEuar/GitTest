3a94b0cc39c8d5fea9b5a997235564aaabbc6735
Eduardo Cruz <edualex152003user@gmail.com>
2023-04-06 11:44:13 -0600

    Removed ¡ from the text function text.

--- js/functions.js
+++ js/functions.js
@@ -2,6 +2,6 @@ const div1 = document.getElementById("div1");
 const testFunction =()=>{
     const newP = document.createElement("p");
     newP.className = "p2"
-    newP.innerHTML = "¡Test function is running!";
+    newP.innerHTML = "Test function is running!";
     div1.appendChild(newP);
 }
