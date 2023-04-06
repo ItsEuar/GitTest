8fe4451ce8314099eb12bae7dd138b47b6481868
Eduardo Cruz <edualex152003user@gmail.com>
2023-04-06 11:41:33 -0600

    Added a button with a test function and style for that button and function.

---
+++ js/functions.js
@@ -0,0 +1,7 @@
+const div1 = document.getElementById("div1");
+const testFunction =()=>{
+    const newP = document.createElement("p");
+    newP.className = "p2"
+    newP.innerHTML = "¡Test function is running!";
+    div1.appendChild(newP);
+}
