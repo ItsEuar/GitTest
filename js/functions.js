bb162377427773e2d8efe7db7a3ded295ca374d1
Eduardo Cruz <edualex152003user@gmail.com>
2023-04-06 13:10:52 -0600

    Fixed infinity button actions and improve button style.

--- js/functions.js
+++ js/functions.js
@@ -1,8 +1,10 @@
 const div1 = document.getElementById("div1");
+const btn = document.getElementById("btn");
 const testFunction =()=>{
     const newP = document.createElement("p");
     newP.className = "p2"
     newP.innerHTML = "Test function is running!";
     div1.appendChild(newP);
     console.log("My name is Eduardo Cruz 😎");
+    btn.disabled = true;
 }
